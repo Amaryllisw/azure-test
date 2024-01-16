3cf16384be9e70d771a870a25a93317be03685a8
Amaryllisw <zhongwenhao123745@gmail.com>
2024-01-16 19:53:05 +0800

    使用https

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
+import { readFileSync } from 'node:fs'
+import https from 'node:https'
+import http from 'node:http'
 import express from 'express'
-import { createWriteStream } from 'node:fs'
+
+const SSL_PATH = '/home/amaryllisw/project/keys/'
+const HOST_NAME = 'amaryllisw.me'
+
+const cert = readFileSync(SSL_PATH + 'cert.crt')
+
+const ca = readFileSync(SSL_PATH + 'ca.crt')
+
+const key = readFileSync(SSL_PATH + 'private.key')
+
+const httpsOptions = {
+    cert,
+    ca,
+    key
+}
+
 
 const app = express()
 
-const ws = createWriteStream('log.txt', { flags: 'a' })
+// 重定义https
+app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+        res.redirect(301, `https://${HOST_NAME}${req.url}`)
+    }
+    next()
+})
 
 app.get('/', (req, res) => {
-    const { id } = req.query
-
-    if (id) {
-        const now = new Date().toLocaleTimeString()
-        ws.write(`time:${now}\nid:${id}\n`)
-        ws.write('------------------\n')
-    }
-
     res.send('lalala')
 })
 
-app.listen(3597, () => { })
+const httpServer = http.createServer(app)
 
-app.on('close', () => {
-    ws.close()
-})
+const httpsServer = https.createServer(httpsOptions, app)
+
+httpServer.listen(80)
+
+httpsServer.listen(443, HOST_NAME)
