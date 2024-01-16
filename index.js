4f6d808c06e1ea33fa7d1e2ae954250576240da1
Amaryllisw <zhongwenhao123745@gmail.com>
2024-01-16 21:35:40 +0800

    https

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ import http from 'node:http'
 import express from 'express'
 
 const SSL_PATH = '/home/amaryllisw/project/keys/'
-const HOST_NAME = 'amaryllisw.me'
 
 const cert = readFileSync(SSL_PATH + 'cert.crt')
 
-// const ca = readFileSync(SSL_PATH + 'ca.crt')
+const ca = readFileSync(SSL_PATH + 'ca.ca-bundle')
 
 const key = readFileSync(SSL_PATH + 'private.key')
 
 const httpsOptions = {
     cert,
-    // ca,
+    ca,
     key
 }
 
 
 const app = express()
+
+app.use(express.static('./public'))
+
 
 // 重定义https
 app.use((req, res, next) => {
@@ -29,7 +31,6 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(express.static('./public'))
 
 const httpServer = http.createServer(app)
 
@@ -37,4 +38,4 @@ const httpsServer = https.createServer(httpsOptions, app)
 
 httpServer.listen(80)
 
-httpsServer.listen(443, HOST_NAME)
+httpsServer.listen(443, '0.0.0.0', () => { console.log('启动成功') })
